6e93aaa281fec85c3d26a87448b5b6760534fef6
jason <jason.c9340@gmail.com>
2023-03-20 12:12:52 +0900

    dialog 내가 공지사항으로 알고있었던것

--- app/src/main/java/com/jason/example/dietmemo/MainActivity.kt
+++ app/src/main/java/com/jason/example/dietmemo/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.jason.example.dietmemo
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import java.util.Calendar
+import java.util.GregorianCalendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +18,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val writeButton = findViewById<ImageView>(R.id.writeBtn)
+        writeButton.setOnClickListener {
+
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("운동 메모 다이얼로그")
+
+
+            val mAlertDialog =  mBuilder.show()
+
+            val DateSelectBtn = mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+
+            mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)?.setOnClickListener {
+
+                val today = GregorianCalendar()
+                val year : Int = today.get(Calendar.YEAR)
+                val month : Int = today.get(Calendar.MONTH)
+                val date : Int = today.get(Calendar.DATE)
+
+                val dlg = DatePickerDialog(this, object  : DatePickerDialog.OnDateSetListener{
+                    override fun onDateSet(view: DatePicker?,year: Int,month: Int,dayOfMonth: Int
+                    ) {
+                        Log.d("main","${year}, ${month+1}, ${dayOfMonth}")
+                        DateSelectBtn?.setText("${year}, ${month+1}, ${dayOfMonth}")
+                    }
+
+                },year, month,date)
+                dlg.show()
+
+            }
+        }
     }
 }
